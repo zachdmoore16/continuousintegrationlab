a54383423beb8125552761a69bf08c2e5e4e959c
Zach Moore <zachdmoore@gmail.com>
2019-11-21 14:52:20 -0500

    Added test page

--- src/main/app.js
+++ src/main/app.js
@@ -7,6 +7,12 @@ app.get('/', (req, res) => {
     res.status(200).send('Hello World!')
 })
 
+// The code below will display 'Hello World!' to the browser when you go to http://localhost:3000
+app.get('/testing', (req, res) => {
+    // the .status(200) isn't necessary since it is the default return status
+    res.status(200).send('This is a test page!')
+})
+
 app.listen(3000, () => {
     console.log('Example app listening on http://localhost:3000')
 })
